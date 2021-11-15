af2bdb68ead5ff7b25d5b592c9c2f77eceb20d68
eliran <eliranmal@gmail.com>
2021-11-15 02:36:12 +0200

    skeleton: dx
    - add convenient dev workflow that is identical to the production 
    environment
    - update lock file
    - update package name

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -6,6 +6,10 @@
 
 import {legacyPlugin} from '@web/dev-server-legacy';
 
+// todo - lookup on how to impl' a dev workflow with dev-server-rollup
+//        https://modern-web.dev/docs/dev-server/plugins/rollup/
+//        for now this dev server is not used (see the 'start' npm script)
+
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
   throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
